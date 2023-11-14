68c6af8c5461689ba2ff7f451564d83f48357052
configid <108707737+configid@users.noreply.github.com>
2023-11-14 23:33:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,97 @@
-function spin() {
-    const spinCount = document.getElementById("spinCount").value || 1;
-    startSpin(spinCount);
-}
-
-function autoSpin(count) {
-    startSpin(count);
-}
-
-function startSpin(count) {
+function spinOnce() {
+    // Randomly select symbols for each slot
     const symbols = ["😎", "😂", "🤣", "😍", "😘", "😅", "🥰", "🤩", "😣", "🥱", "😴", "😛", "😜", "😭", "🤑"];
-    const slot1 = document.getElementById("slot1");
-    const slot2 = document.getElementById("slot2");
-    const slot3 = document.getElementById("slot3");
-    const resultDiv = document.getElementById("result");
+    const symbolMatrix = [
+        [symbols[Math.floor(Math.random() * symbols.length)], symbols[Math.floor(Math.random() * symbols.length)], symbols[Math.floor(Math.random() * symbols.length)]],
+        [symbols[Math.floor(Math.random() * symbols.length)], symbols[Math.floor(Math.random() * symbols.length)], symbols[Math.floor(Math.random() * symbols.length)]],
+        [symbols[Math.floor(Math.random() * symbols.length)], symbols[Math.floor(Math.random() * symbols.length)], symbols[Math.floor(Math.random() * symbols.length)]]
+    ];
 
-    // Disable the button during the spin
-    document.querySelector("button").disabled = true;
+    // Display symbols
+    slot1.textContent = symbolMatrix[0][0];
+    slot2.textContent = symbolMatrix[0][1];
+    slot3.textContent = symbolMatrix[0][2];
+    slot4.textContent = symbolMatrix[1][0];
+    slot5.textContent = symbolMatrix[1][1];
+    slot6.textContent = symbolMatrix[1][2];
 
-    // Add a class to trigger the roll animation
-    slot1.classList.add("rolling");
-    slot2.classList.add("rolling");
-    slot3.classList.add("rolling");
+    // Check for a win
+    const winSymbol = checkForWin(symbolMatrix);
+    if (winSymbol) {
+        // Logika kemenangan di sini, sesuai simbol yang menang
+        const winnings = calculateWinnings(winSymbol); // Menghitung kemenangan berdasarkan simbol
 
-    let spinsLeft = count;
+        resultDiv.textContent = `Congratulations! You won ${winnings} coins with ${winSymbol} combination.`;
+    } else {
+        resultDiv.textContent = "Sorry, try again!";
+    }
 
-    function spinOnce() {
-        // Randomly select symbols for each slot
-        const symbol1 = symbols[Math.floor(Math.random() * symbols.length)];
-        const symbol2 = symbols[Math.floor(Math.random() * symbols.length)];
-        const symbol3 = symbols[Math.floor(Math.random() * symbols.length)];
+    spinsLeft--;
 
-        // Display symbols
-        slot1.textContent = symbol1;
-        slot2.textContent = symbol2;
-        slot3.textContent = symbol3;
+    if (spinsLeft > 0) {
+        // Continue spinning after a delay
+        setTimeout(spinOnce, 2000);
+    } else {
+        // Remove the class to end the roll animation
+        slot1.classList.remove("rolling");
+        slot2.classList.remove("rolling");
+        slot3.classList.remove("rolling");
+        slot4.classList.remove("rolling");
+        slot5.classList.remove("rolling");
+        slot6.classList.remove("rolling");
 
-        // Check for a win
-        if (symbol1 === symbol2 && symbol2 === symbol3) {
-            const multiplier = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 20, 25, 50, 100, 125, 150, 200, 250, 500];
-            const randomMultiplier = multiplier[Math.floor(Math.random() * multiplier.length)];
-            const winnings = randomMultiplier * 10; // Assuming the bet is 10 (can be adjusted)
+        // Enable the button after the spins are complete
+        document.querySelector("button").disabled = false;
+    }
+}
 
-            resultDiv.textContent = `Congratulations! You won ${winnings} coins with a ${randomMultiplier}x multiplier.`;
-        } else {
-            resultDiv.textContent = "Sorry, try again!";
-        }
-
-        spinsLeft--;
-
-        if (spinsLeft > 0) {
-            // Continue spinning after a delay
-            setTimeout(spinOnce, 2000);
-        } else {
-            // Remove the class to end the roll animation
-            slot1.classList.remove("rolling");
-            slot2.classList.remove("rolling");
-            slot3.classList.remove("rolling");
-
-            // Enable the button after the spins are complete
-            document.querySelector("button").disabled = false;
+function checkForWin(matrix) {
+    // Check for horizontal wins
+    for (let i = 0; i < matrix.length; i++) {
+        const row = matrix[i];
+        if (checkRowWin(row)) {
+            return row[0];
         }
     }
 
-    // Start the first spin
-    spinOnce();
+    // Check for vertical wins
+    for (let j = 0; j < matrix[0].length; j++) {
+        const column = [matrix[0][j], matrix[1][j], matrix[2][j]];
+        if (checkRowWin(column)) {
+            return column[0];
+        }
+    }
+
+    return null; // No win
+}
+
+function checkRowWin(row) {
+    const symbolCount = {};
+    for (const symbol of row) {
+        symbolCount[symbol] = (symbolCount[symbol] || 0) + 1;
+    }
+
+    for (const symbol in symbolCount) {
+        if (symbolCount[symbol] >= 8) {
+            return true; // Win if there are 8 or more of the same symbol in a row
+        }
+    }
+
+    return false;
+}
+
+function calculateWinnings(winSymbol) {
+    // Logika untuk menghitung kemenangan berdasarkan simbol
+    // Misalnya, Anda dapat mengatur nilai kemenangan berdasarkan simbol tertentu
+    const symbolValues = {
+        "😎": 100,
+        "😂": 50,
+        "🤣": 30,
+        "😍": 20,
+        "😘": 10,
+        "😅": 5,
+        // ... tambahkan simbol lain sesuai kebutuhan
+    };
+
+    return symbolValues[winSymbol] || 0;
 }
