384e3c275fa6d39341dacb8e391a8d6c44ba6b3f
configid <108707737+configid@users.noreply.github.com>
2023-11-14 22:57:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-let rotating = false;
+function spin() {
+    const symbols = ["😎", "😂", "🤣", "😍", "😘", "😅", "🥰", "🤩", "😣", "🥱", "😴", "😛", "😜", "😭", "🤑"];
+    const slot1 = document.getElementById("slot1");
+    const slot2 = document.getElementById("slot2");
+    const slot3 = document.getElementById("slot3");
+    const resultDiv = document.getElementById("result");
 
-function spinWheel() {
-  if (!rotating) {
-    rotating = true;
-    const wheel = document.getElementById('wheel');
-    const randomRotation = Math.floor(Math.random() * 360) + 360 * 5;
-    
-    wheel.style.transition = 'transform 3s ease-out';
-    wheel.style.transform = `rotate(${randomRotation}deg)`;
-    
-    setTimeout(() => {
-      wheel.style.transition = 'none';
-      rotating = false;
-    }, 3000);
-  }
+    // Randomly select symbols for each slot
+    const symbol1 = symbols[Math.floor(Math.random() * symbols.length)];
+    const symbol2 = symbols[Math.floor(Math.random() * symbols.length)];
+    const symbol3 = symbols[Math.floor(Math.random() * symbols.length)];
+
+    // Display symbols
+    slot1.textContent = symbol1;
+    slot2.textContent = symbol2;
+    slot3.textContent = symbol3;
+
+    // Check for a win
+    if (symbol1 === symbol2 && symbol2 === symbol3) {
+        const multiplier = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 20, 25, 50, 100, 125, 150, 200, 250, 500];
+        const randomMultiplier = multiplier[Math.floor(Math.random() * multiplier.length)];
+        const winnings = randomMultiplier * 10; // Assuming the bet is 10 (can be adjusted)
+
+        resultDiv.textContent = `Congratulations! You won ${winnings} coins with a ${randomMultiplier}x multiplier.`;
+    } else {
+        resultDiv.textContent = "Sorry, try again!";
+    }
 }
